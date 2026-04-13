70e039bd3e943532a647083736ae1161840a4e6a
Ritik <ritikrajg@gmail.com>
2026-04-13 17:20:44 +0530

    Transaction Validation

--- src/com/iispl/repository/TransactionRepoImpl.java
+++ src/com/iispl/repository/TransactionRepoImpl.java
@@ -12,12 +12,24 @@ public class TransactionRepoImpl implements TransactionRepo{
     public TransactionRepoImpl() {
         this.transactions = new ArrayList<>();
     }
-
+    
+    @Override
     public void addTransaction(Transaction transaction) {
         this.transactions.add(transaction);
     }
 
+    @Override
     public List<Transaction> findAllTransactions() {
         return this.transactions;
+    }
+
+    @Override
+    public boolean existsTransactionId(String transactionId) {
+        for (Transaction t : transactions) {
+            if (t.getTransactionId().equals(transactionId)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
